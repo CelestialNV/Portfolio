ac18b7f8948f062a7f12c1d97885913e82727694
CelestialNV <khaldi.achraf500@gmail.com>
2023-01-09 00:01:56 +0100

    Header changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
-import photo from '../ressources/photo2.png';
 
 function Header(){
     return(
         <div className="bg-[url('./ressources/header-bg.jpg')] ">
       <div className='pr-20 pb-40 pt-10 font-Alata'>
-      <img src={photo}/>
-          <div className='text-3xl text-secondary text-right font-Alata'>
+          <div className='text-3xl text-secondary text-center font-Alata'>
             Hi there ! My name is
           </div>
-        <div className='text-9xl text-white text-right font-Alata'> 
+        <div className='text-9xl text-white text-center font-Alata'> 
             KHALDI <br/> ACHRAF
           </div>
-        <div className='text-3xl text-white text-right font-Alata'>
+        <div className='text-3xl text-white text-center font-Alata'>
              <div className=''>And I'm a <div className='inline text-secondary'>computer engineer</div> <br/> and an aspiring game developer</div>
         </div>
       </div>
